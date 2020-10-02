98508b0fb2501b971a243b708084c1c90880fd59
Jens Ochsenmeier <github@jens-ox.de>
2020-10-02 12:25:18 +0200

    functional components by default

--- boilerplate/src/routes/Home.tsx
+++ boilerplate/src/routes/Home.tsx
@@ -1,63 +1,50 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) 2020 Daimler TSS GmbH
 
-import { createStyles, Theme, withStyles, WithStyles, Container, Typography, Box, Button } from '@material-ui/core';
-import { I18nService, inject, withInject, RouterService, ConfigService } from '@daimler/ftk-core';
+import { Theme, Container, Typography, Box, Button, makeStyles } from '@material-ui/core';
+import { useI18n, useConfig, useRouter } from '@daimler/ftk-core';
 import * as React from 'react';
 
 import Logo from '../components/Logo';
 import ProjectConfig from '../globals/interfaces/ProjectConfig';
 
-const HomeStyles = ({ spacing, palette }: Theme) =>
-  createStyles({
-    congratulation: {
-      color: palette.primary.light,
-      margin: spacing(1),
-      textAlign: 'center',
-    },
-    centered: {
-      textAlign: 'center',
-    },
-  });
+const useStyles = makeStyles(({ spacing, palette }: Theme) => ({
+  congratulation: {
+    color: palette.primary.light,
+    margin: spacing(1),
+    textAlign: 'center',
+  },
+  centered: {
+    textAlign: 'center',
+  },
+}));
 
-class Home extends React.Component<WithStyles<typeof HomeStyles>, {}> {
-  @inject()
-  public i18n!: I18nService;
+const Home = () => {
+  const classes = useStyles();
+  const i18n = useI18n();
+  const config = useConfig();
+  const router = useRouter();
 
-  @inject()
-  public router!: RouterService;
+  return (
+    <Container>
+      <Logo />
+      <Typography variant={'h5'} gutterBottom={true} align="center">
+        {i18n.translateToString('Headline', {
+          appName: config.getConfig<ProjectConfig, {}>().project.appName,
+        })}
+      </Typography>
 
-  @inject()
-  public config!: ConfigService;
+      <Typography variant={'h6'} className={classes.congratulation}>
+        {i18n.translate('Congratulation')}
+      </Typography>
+      <Typography className={classes.centered}>{i18n.translate('DescriptionStart')}</Typography>
+      <Box m={3} className={classes.centered}>
+        <Button variant="contained" color="secondary" onClick={() => router.navigate(router.linkTo('demoContent'))}>
+          {i18n.translateToString('ReadMore')}
+        </Button>
+      </Box>
+    </Container>
+  );
+};
 
-  public render(): JSX.Element {
-    const { classes } = this.props;
-
-    return (
-      <Container>
-        <Logo />
-        <Typography variant={'h5'} gutterBottom={true} align="center">
-          {this.i18n.translateToString('Headline', {
-            appName: this.config.getConfig<ProjectConfig, {}>().project.appName,
-          })}
-        </Typography>
-
-        <Typography variant={'h6'} className={classes.congratulation}>
-          {this.i18n.translate('Congratulation')}
-        </Typography>
-        <Typography className={classes.centered}>{this.i18n.translate('DescriptionStart')}</Typography>
-        <Box m={3} className={classes.centered}>
-          <Button variant="contained" color="secondary" onClick={this.onReadMore}>
-            {this.i18n.translateToString('ReadMore')}
-          </Button>
-        </Box>
-      </Container>
-    );
-  }
-
-  private onReadMore = () => {
-    this.router.navigate(this.router.linkTo('demoContent'));
-  };
-}
-
-export default withStyles(HomeStyles, { withTheme: true })(withInject(Home));
+export default Home;
