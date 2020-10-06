a4028c305a10323b5397e7465006548f9a7c3002
Jens Ochsenmeier <github@jens-ox.de>
2020-10-06 15:38:18 +0200

    Revert "remove createStyles duplications"
    This reverts commit fce5c9fc7292f244ecd9d336f331eb4a57d2c1c9.

--- boilerplate/src/components/LanguageSwitch.tsx
+++ boilerplate/src/components/LanguageSwitch.tsx
@@ -2,43 +2,46 @@
 // Copyright (c) 2020 Daimler TSS GmbH
 
 import * as React from 'react';
-import { Typography, Button, makeStyles } from '@material-ui/core';
+import { Typography, Button } from '@material-ui/core';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 
 import LanguageIcon from '@material-ui/icons/Language';
 import CompareArrowsIcon from '@material-ui/icons/CompareArrows';
 import { useI18n } from '@daimler/ftk-core';
 
-const useStyles = makeStyles(({ palette }) => ({
-  languageToggle: {
-    position: 'relative',
+const useStyles = makeStyles(({ palette }: Theme) =>
+  createStyles({
+    languageToggle: {
+      position: 'relative',
 
-    '& div': {
-      display: 'none',
-      position: 'absolute',
-      top: '0',
-      right: '100%',
-      whiteSpace: 'nowrap',
-      background: palette.grey[700],
-      padding: '7px 8px',
+      '& div': {
+        display: 'none',
+        position: 'absolute',
+        top: '0',
+        right: '100%',
+        whiteSpace: 'nowrap',
+        background: palette.grey[700],
+        padding: '7px 8px',
 
-      '& p': {
-        flexDirection: 'column',
-        padding: '0 8px 0 0',
-        fontSize: '1rem',
-        lineHeight: '1.5',
+        '& p': {
+          flexDirection: 'column',
+          padding: '0 8px 0 0',
+          fontSize: '1rem',
+          lineHeight: '1.5',
+        },
+      },
+      '&:hover': {
+        background: palette.grey[700],
+      },
+      '&:hover div': {
+        display: 'flex',
       },
     },
-    '&:hover': {
-      background: palette.grey[700],
+    languageLabel: {
+      padding: '2px 0 0 5px',
     },
-    '&:hover div': {
-      display: 'flex',
-    },
-  },
-  languageLabel: {
-    padding: '2px 0 0 5px',
-  },
-}));
+  }),
+);
 
 export default function LanguageSwitch(props: unknown) {
   const classes = useStyles(props);
--- boilerplate/src/components/Logo.tsx
+++ boilerplate/src/components/Logo.tsx
@@ -2,21 +2,23 @@
 // Copyright (c) 2020 Daimler TSS GmbH
 
 import * as React from 'react';
-import { makeStyles } from '@material-ui/core';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import imgLogo from '../assets/images/logo.png';
 
-const useStyles = makeStyles(({ spacing }) => ({
-  logo: {
-    margin: `${spacing(2)}px ${spacing(5)}px ${spacing(5)}px ${spacing(5)}px`,
-    textAlign: 'center',
-    '& img': {
-      maxWidth: '200px',
+const useStyles = makeStyles(({ spacing }: Theme) =>
+  createStyles({
+    logo: {
+      margin: `${spacing(2)}px ${spacing(5)}px ${spacing(5)}px ${spacing(5)}px`,
+      textAlign: 'center',
+      '& img': {
+        maxWidth: '200px',
+      },
+      '&:hover': {
+        opacity: 0.9,
+      },
     },
-    '&:hover': {
-      opacity: 0.9,
-    },
-  },
-}));
+  }),
+);
 
 export default function Logo(props: unknown) {
   const classes = useStyles(props);
